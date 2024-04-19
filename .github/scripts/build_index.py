39e328ee0659dc083968754fb08311408ff4f515
Carlos Vivar Rios <carlosvivarrios@gmail.com>
2024-04-19 18:45:30 +0200

    Updating script and adding it to gh-page

--- .github/scripts/build_index.py
+++ .github/scripts/build_index.py
@@ -28,7 +28,7 @@ def read_component(file: Path):
     except:
         # add "added": "YYYY-MM-DD"
         component["added"] = str(datetime.datetime.now().date())
-        component["odpt.yaml"] = f"{component['component-name']}_{component['component-version']}.yaml",
+        component["odpt.yaml"] = f"{component['component-name']}_{component['component-version']}.yaml"
         # with open(file, 'w', encoding='utf-8') as f:
         #     yaml.dump(component, f, default_flow_style=False)
 
